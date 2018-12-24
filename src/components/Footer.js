0092c7f988f4db02f4a0c6ca264edb1382fb31cb
Pauloalgelin <pauloalgelin@gmail.com>
2018-12-24 03:13:11 -0200

    Parent/child components.

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Footer() {
+    return (
+        <footer>My footer</footer>
+    )
+}
+
+export default Footer
---
+++ src/components/MainContent.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function MainContent() {
+    return (
+        <main>
+            <h3>You know me</h3>
+            <ul>
+                <li>That place</li>
+                <li>That job</li>
+                <li>That automobile</li>
+            </ul>
+        </main>
+    )
+}
+
+export default MainContent
